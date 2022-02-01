dece88c6dd7fe5768149803ff6cb713341315c5e
Daniel <Daniel@DESKTOP-FQNQR1B.Home>
2022-02-01 20:41:10 +0100

    3 - Añadido Dominio + formateaNombre

--- src/test/java/org/iesalandalus/programacion/reservasaulas/mvc/modelo/dominio/AulaTest.java
+++ src/test/java/org/iesalandalus/programacion/reservasaulas/mvc/modelo/dominio/AulaTest.java
@@ -8,7 +8,6 @@ import static org.junit.Assert.fail;
 import org.junit.Test;
 
 public class AulaTest {
-	/*
 	private static final String ERROR_EXCEPCION = "Debería haber saltado la excepción.";
 	private static final String ERROR_NO_EXCEPCION = "No debería haber saltado la excepción.";
 	private static final String NOMBRE = "Salón de actos";
@@ -102,5 +101,4 @@ public class AulaTest {
 		Aula aula = new Aula(NOMBRE);
 		assertEquals("nombre Aula=Salón de actos", aula.toString());
 	}
-*/
 }
